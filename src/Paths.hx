8aaf048d6a984aeac76036f89774f0654285c3fa
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-04 19:16:59 -0400

    epic

--- src/Paths.hx
+++ src/Paths.hx
@@ -1,8 +1,31 @@
 package;
 
 import fileUtil.FileTypes;
+import openfl.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 
 class Paths
 {
-    
+    // will test soon
+    public static inline function getIso(file:String, path:String, ?directory:String){
+        if (Assets.exists(file) && path != '' || path != null){
+            return file;
+        }
+        else if (Assets.exists(file) && path != null && directory != null){
+            var fileToReturn:String = path + '/' + directory;
+            return fileToReturn;
+        }
+        else if (!Assets.exists(file)){
+            return null;
+        }
+        else if (directory == null){
+            var fileToReturn:String = path;
+            return fileToReturn;
+            trace('Directory not found');
+        }
+    }
 }
--- src/fileUtil/FileFinder.hx
+++ src/fileUtil/FileFinder.hx
@@ -1,8 +1,22 @@
 package fileUtil;
 
 import fileUtil.FileTypes;
+import openfl.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 
 class FileFinder
 {
+    public var file:Dynamic;
 
+    public var path:String;
+
+    if (Assets.exists(file) && Assets.exists(path)){
+        if (file.endsWith(".iso")){
+            return Paths.getIso(file, path);
+        }
+    }
 }
