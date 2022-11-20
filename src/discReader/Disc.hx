984e34a9383a443e6158827e9181f813bee58358
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-20 00:51:22 -0500

    Update Disc.hx

--- src/discReader/Disc.hx
+++ src/discReader/Disc.hx
@@ -5,5 +5,17 @@ import discReader.DiscDetector;
 
 class Disc extends DiscReader
 {
-    
+    var length:Float = 0; // song length or whatever shit is playing
+
+    public function new(){
+        super();
+    }
+
+    override function create(){
+        super.create();
+    }
+
+    override function update(elapsed:Float){
+        super.update(elapsed);
+    }
 }
